264730f911906f7932ebba3b28eadccf0b8a1354
hyperit123 <heath.w.r@outlook.com>
2025-09-03 22:31:42 +0200

    making it more like the jack-knife website by addign the import of pfp and texst stuffs and test of checkbox thingi

--- script.js
+++ script.js
@@ -2,22 +2,61 @@ let checkbox = 1;
 const addCheckbox = document.getElementById('addCheckbox');
 const removeCheckbox = document.getElementById('removeCheckbox');
 const checkboxContainer = document.getElementById('checkboxContainer');
+//let cbgName = document.getElementById('cbgName').value;
 
-addCheckbox.addEventListener('click', function() {
+addCheckbox.onclick = function() {
     checkbox++;
     const newCheckbox = document.createElement('input');
     newCheckbox.type = 'checkbox';
     newCheckbox.id = 'checkbox' + checkbox;
     checkboxContainer.appendChild(newCheckbox);
-});
+    // Add a line break after every 10th checkbox
+    //if (checkbox % 10 === 0) {
+    //const lineBreak = document.createElement('br');
+    //    checkboxContainer.appendChild(lineBreak);
+    //}
+};
 
-removeCheckbox.addEventListener('click', function() {
+removeCheckbox.onclick = function() {
     if (checkbox > 1) {
-        checkbox--;
+        // Remove the last element (which might be a checkbox or a <br>)
         checkboxContainer.removeChild(checkboxContainer.lastChild);
+
+        checkbox--;
+        
+        // If we just removed a line break, also remove the checkbox
+        //if (checkbox % 10 === 0 && checkboxContainer.lastChild.tagName === 'INPUT') {
+            // We removed a <br>, now remove the checkbox too
+        //} 
+        //else if (checkbox % 10 === 0) {
+        //    // We removed a checkbox that was followed by a <br>, so remove the <br> too
+        //    if (checkboxContainer.lastChild && checkboxContainer.lastChild.tagName === 'BR') {
+        //        checkboxContainer.removeChild(checkboxContainer.lastChild);
+        //    }
+        //}
+        //if (checkboxContainer.lastChild.tagName === 'BR') {
+        //    checkbox = checkbox; 
+        //}
+        //else {
+        //    checkbox--;
+        //}
     }
     else {
         alert('Cannot remove the last checkbox.');
     }
-});
+};
 
+const pfpImport = document.getElementById('pfpImport');
+pfpImport.onclick = function() {
+    document.getElementById('pfpInput').click();
+};
+
+const pfpInput = document.getElementById('pfpInput').addEventListener('change', function() {
+    if (this.files && this.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('pfp').src = e.target.result;
+        };
+        reader.readAsDataURL(this.files[0]);
+    }
+});
